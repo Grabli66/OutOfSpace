cc719f460b08f7bc61e9cdf18d3fb4399243a5cd
Grabli66 <grabli66@gmail.com>
2022-04-19 07:44:31 +0300

    Перенесён дистрибутив

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 module.exports = {
   entry: "./src/index.ts",
   output: {
-    path: path.resolve(__dirname, "./dist"),
+    path: path.resolve(__dirname, "./docs"),
     filename: '[name].[contenthash].js',
   },
   resolve: {
